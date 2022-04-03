202c3db11681e28415ec62f11b8f6abb47c73aa4
pilming44 <pilming44@naver.com>
2022-04-04 00:15:40 +0900

    Entity클래스 개발중

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "mamber_id")
+    private Long id;
+
+    private String naem;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> oreders = new ArrayList<>();
+}
