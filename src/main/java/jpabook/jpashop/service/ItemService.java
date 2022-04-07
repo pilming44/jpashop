f01996d5fc84be394729b0a6d041361f63503f3e
pilming44 <pilming44@naver.com>
2022-04-07 22:33:28 +0900

    상품 도메인 개발 - 상품 엔티티에 비즈니스 로직 추가 - 상품 리포지토리 개발 - 상품 서비스 개발

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional//리드온리기때문에 덮어씌우지않으면 저장이 안됨.
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
