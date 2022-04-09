31369f6ea93aebd542790df2f3ab8a1da88e46ef
pilming44 <pilming44@naver.com>
2022-04-09 17:40:54 +0900

    웹 계층 개발 - 홈 화면과 레이아웃 - 회원 등록 (validation관련)

---
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+public class MemberForm {
+
+    // TODO: 2022-04-09  javax.validation공부하기 훨신 코드가 간편해짐 타임리프와 세트
+    @NotEmpty(message = "회원 이름은 필수 입니다.")
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
